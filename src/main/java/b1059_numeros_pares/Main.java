4e43e0ddfd7e891e9841b022b2cd3d5bc626b6ff
Alexandre Froes <alexandre.froes@estudante.iftm.edu.br>
2024-04-15 23:47:28 -0300

    Exercicio 1059 feito.

---
+++ src/main/java/b1059_numeros_pares/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        for ( int num = 2 ; num <= 100 ; num = num + 2 ) {
+            System.out.println(num);
+        }
+    }
+}
