9d7b36c6cf4974b393bab64961f1cb365c948c57
Xande <Alexandre.froes@estudante.iftm.edu.br>
2024-09-30 19:20:16 -0300

    oi matheus

--- algoritmos/beecrowd/REC01_gerar_repeticao/Main.java
+++ algoritmos/beecrowd/REC01_gerar_repeticao/Main.java
@@ -3,12 +3,17 @@ package REC01_gerar_repeticao;
 import java.util.Scanner;
 
 public class Main {
-    static int repeticaoN(int n, int qtd) {
+    static int repeticaoN(int n, int qtd) {    
+        if (qtd == 0) {
+            System.out.println("");
+            return 0;
+        }
+
         System.out.printf("%d", n);
-        
+
         if (qtd <= 1) {
-            System.out.println();
-            return n;
+            System.out.println("");
+            return 0;
         } else {
             System.out.printf("-");
             return repeticaoN(n, qtd - 1);
