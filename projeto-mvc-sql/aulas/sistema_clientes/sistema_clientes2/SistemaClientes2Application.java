0860b1b1fa9eaca373c8365f972c50aa35bd2af9
Xande <160709641+Alexandre-Froes@users.noreply.github.com>
2024-10-28 21:59:51 -0300

    jkadka
    adad

---
+++ projeto-mvc-sql/aulas/sistema_clientes/sistema_clientes2/SistemaClientes2Application.java
@@ -0,0 +1,13 @@
+package br.iftm.xande.sistema_clientes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClientes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClientes2Application.class, args);
+	}
+
+}
---
+++ projeto-mvc-sql/aulas/sistema_clientes/src/main/java/br/edu/iftm/tspi/pmvc/sistema_clientes/SistemaClientes2Application.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.pmvc.sistema_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClientes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClientes2Application.class, args);
+	}
+
+}
