ce3a07941383d254eb553d628b81f43092811b14
Xande <Alexandre.froes@estudante.iftm.edu.br>
2024-11-06 12:26:12 -0300

    arrumei kkk

--- poo1/java/encapsulamento2/App.java
+++ poo1/java/encapsulamento2/App.java
@@ -6,35 +6,32 @@ public class App {
     public static JogadorDeFutebol leDados() {
         Scanner ler = new Scanner(System.in);
         JogadorDeFutebol jogador = new JogadorDeFutebol();
-
-        System.out.println("Digite o nome do jogador:");
-        jogador.setNome(ler.nextLine());
-        
+    
         System.out.println("Digite a nacionalidade do jogador:");
-        jogador.setNacionalidade(ler.nextLine());
-        
+        jogador.nacionalidade = ler.nextLine();
+    
         System.out.println("Digite a data de nascimento do jogador (somente números) na seguinte sequência ddmmaaaa:");
-        jogador.setDtNasc(ler.nextInt());
+        jogador.dtNasc = ler.nextInt();
         ler.nextLine();
-        
+    
         System.out.println("Digite a posição do jogador:");
-        jogador.setPosic(ler.nextLine());
-        
+        jogador.posic = ler.nextLine();
+    
         System.out.println("Digite o peso do jogador:");
-        jogador.setPeso(ler.nextDouble());
+        jogador.peso = ler.nextDouble();
         ler.nextLine();
-        
+    
         System.out.println("Digite a altura do jogador:");
-        jogador.setAltura(ler.nextDouble());
+        jogador.altura = ler.nextDouble();
         ler.nextLine();
-        
+    
         ler.close();
-
+    
         return jogador;
     }
-
+    
     public static void tempoAposentadoria(JogadorDeFutebol jogador) {
-        String posicao = jogador.getPosic().toLowerCase();
+        String posicao = jogador.posic.toLowerCase();
         int idade = jogador.calculaIdade();
         int idadeAposentadoria = 0;
     
@@ -54,25 +51,23 @@ public class App {
         }
     
         if (idade >= idadeAposentadoria) {
-            System.out.println("Jogador " + jogador.getNome() + "você pode se aposentar!");
+            System.out.println("Jogador " + jogador.getNome() + ", você pode se aposentar!");
         } else {
-            System.out.printf("Jogador %s você ainda não pode se aposentar! Faltam %d ano(s).\n",jogador.getNome(), idadeAposentadoria - idade);
+            System.out.printf("Jogador %s, você ainda não pode se aposentar! Faltam %d ano(s).\n", jogador.getNome(), idadeAposentadoria - idade);
         }
     }
-        
+    
     public static String exibe(JogadorDeFutebol jogador) {
-        int ano = jogador.getDtNasc() % 10000;
-
-        int mes = (jogador.getDtNasc() / 10000) % 100;
-
-        int dia = jogador.getDtNasc() / 1000000;
-
-    return "Nome: " + jogador.getNome() + "\n" +
-            "Data de Nascimento: " + String.format("%02d/%02d/%d", dia, mes, ano) + "\n" +
-            "Altura: " + String.format("%.2f", jogador.getAltura()) + " m\n" +
-            "Peso: " + String.format("%.1f", jogador.getPeso()) + " kg\n" +
-            "Nacionalidade: " + jogador.getNacionalidade() + "\n" +
-            "Posição: " + jogador.getPosic() + "\n";
+        int ano = jogador.dtNasc % 10000;
+        int mes = (jogador.dtNasc / 10000) % 100;
+        int dia = jogador.dtNasc / 1000000;
+    
+        return "Nome: " + jogador.getNome() + "\n" +
+                "Data de Nascimento: " + String.format("%02d/%02d/%d", dia, mes, ano) + "\n" +
+                "Altura: " + String.format("%.2f", jogador.altura) + " m\n" +
+                "Peso: " + String.format("%.1f", jogador.peso) + " kg\n" +
+                "Nacionalidade: " + jogador.nacionalidade + "\n" +
+                "Posição: " + jogador.posic + "\n";
     }
 
     public static void main(String[] args) {
--- poo1/java/encapsulamento2/JogadorDeFutebol.java
+++ poo1/java/encapsulamento2/JogadorDeFutebol.java
@@ -4,11 +4,11 @@ import java.util.Calendar;
 
 public class JogadorDeFutebol {
     private String nome;
-    private String nacionalidade;
-    private int dtNasc;
-    private String posic;
-    private double peso;
-    private double altura;
+    public String nacionalidade;
+    public int dtNasc;
+    public String posic;
+    public double peso;
+    public double altura;
 
     public JogadorDeFutebol(String nome, String nacionalidade, int dtNasc, String posic, double peso, double altura) {
         this.nome = nome;
@@ -27,41 +27,6 @@ public class JogadorDeFutebol {
     }
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
-    }
-
-    public int getDtNasc() {
-        return dtNasc;
-    }
-    public void setDtNasc(int dtNasc) {
-        this.dtNasc = dtNasc;
-    }
-
-    public String getPosic() {
-        return posic;
-    }
-    public void setPosic(String posic) {
-        this.posic = posic;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-    public void setAltura(double altura) {
-        this.altura = altura;
     }
 
     public int calculaIdade() {
