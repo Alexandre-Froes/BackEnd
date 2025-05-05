4c7d712d75f39bd91b65177edc83c07904aa7b5c
Xande <160709641+Alexandre-Froes@users.noreply.github.com>
2025-05-05 20:45:05 -0300

    Envio da atividade A4 (ou a3) de testes automatizados

---
+++ testesAuto/atividade-a2/tdd/src/main/java/iftm/tspi/ta/xande/tdd/Funcionario.java
@@ -0,0 +1,40 @@
+package iftm.tspi.ta.xande.tdd;
+
+public class Funcionario {
+    private String nome;
+    private Integer horasTrabalhadas;
+    private Double valorHora;
+
+    public Funcionario(String nome, Integer horasTrabalhadas, Double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+    public Funcionario() {
+    }
+    public String getNome() {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+
+    public void setNome(String nome) {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+
+    public Integer getHorasTrabalhadas() {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+
+    public void setHorasTrabalhadas(Integer horasTrabalhadas) {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+
+    public Double getValorHora() {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+
+    public void setValorHora(Double valorHora) {
+        throw new UnsupportedOperationException("Metodo ainda não implementado.");
+    }
+    
+    
+}
