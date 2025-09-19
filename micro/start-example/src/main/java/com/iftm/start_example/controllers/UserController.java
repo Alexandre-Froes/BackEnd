cd37c78658a1db8cf4c81ea8d7f1894d560b3eb6
Xande <160709641+Alexandre-Froes@users.noreply.github.com>
2025-09-18 21:55:37 -0300

    Upload da E3 de microsservicos
    Sem application properties

--- micro/start-example/src/main/java/com/iftm/start_example/controllers/UserController.java
+++ micro/start-example/src/main/java/com/iftm/start_example/controllers/UserController.java
@@ -2,26 +2,56 @@ package com.iftm.start_example.controllers;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.iftm.start_example.services.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.iftm.start_example.models.User;
 import com.iftm.start_example.models.dto.UserDto;
+import com.iftm.start_example.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
-    @Autowired
-    private UserService service;
 
-    @GetMapping()
+    @Autowired
+    private UserService service = new UserService();
+
+    @GetMapping
     public ResponseEntity<List<UserDto>>findAll() {
         return service.findAll();
     }
     
+    @GetMapping("/id/{id}")
+    public ResponseEntity<UserDto> findById
+        (@PathVariable("id") ObjectId id) {
+        return service.findById(id);
+    }
 
+    @PostMapping()
+    public ResponseEntity<UserDto> create
+        (@RequestBody User user) {
+        return service.save(user);
+    }
+    
+    @PutMapping
+    public ResponseEntity<UserDto> update
+        (@RequestBody UserDto user) {
+        return service.update(user);
+    }
+
+    @DeleteMapping("id/{id}")
+    public ResponseEntity<Object> delete
+        (@PathVariable("id") ObjectId id) {
+        return service.delete(id);
+    }
 }
--- micro/start-example/src/main/java/com/iftm/start_example/services/UserService.java
+++ micro/start-example/src/main/java/com/iftm/start_example/services/UserService.java
@@ -5,11 +5,15 @@ import java.util.stream.Collectors;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import com.iftm.start_example.models.User;
 import com.iftm.start_example.models.dto.UserDto;
 import com.iftm.start_example.repositories.UserRepository;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository repository;
@@ -39,6 +43,42 @@ public class UserService {
         
 
         return ResponseEntity.ok(new UserDto(dbUser.get()));
+    }
+
+    public ResponseEntity<UserDto> save(User user) {
+        if(user.getName().isBlank() || user.getAge() <= 0)
+            return ResponseEntity.badRequest().build();
+
+        user.setId(ObjectId.get());
+        return ResponseEntity.ok(new UserDto(repository.save(user)));
+    }
+
+    public ResponseEntity<UserDto> update(UserDto user) {
+        if(user.getId() == null)
+            return ResponseEntity.badRequest().build();
+
+        var objectId = new ObjectId(user.getId());
+        var dbUser = repository.findById(objectId);
+        if(dbUser.isEmpty()) 
+            return ResponseEntity.badRequest().build();
+        
+        var dbUserObj = dbUser.get();
+        dbUserObj.setName(user.getName());
+        dbUserObj.setAge(user.getAge());
+        return ResponseEntity.ok(new UserDto(repository.save(dbUserObj)));
+    }
+
+    public ResponseEntity<Object> delete(ObjectId id) {
+        if(id == null)
+            return ResponseEntity.badRequest().build();
+        repository.deleteById(id);
+
+        var dbUser = repository.findById(id);
+        if(dbUser.isPresent())
+            return ResponseEntity.status
+                (HttpStatus.NOT_MODIFIED).build();
+        
+        return ResponseEntity.ok().build();
     }
 }
 
